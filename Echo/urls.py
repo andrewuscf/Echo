aa0fc1252ee4afb4f5c5591d796edd9f47ab9896
Andrew Saldana <andrewuscf@hotmail.com>
2015-02-23 15:45:38 -0800

    fixed files

--- Echo/urls.py
+++ Echo/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from Main import views
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Echo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
     url(r'^api/', include('Main.api.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
