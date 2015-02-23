165d9a8f14fa3dfc0f384c1ce3ce83b715c36d36
Andrew Saldana <andrewuscf@hotmail.com>
2015-02-23 14:51:16 -0800

    all working

---
+++ Main/static/js/app.js
@@ -0,0 +1,5 @@
+ var echoApp = angular.module('echoApp', ['echo-directives','djangular']);
+
+echoApp.controller('EchoController', function ($scope, $http){ // calling the app controller up to the http
+
+});
