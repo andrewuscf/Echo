f4507a7c75eb03572f960ad00428a21617c83a30
Daniel Gin <dahgin@gmail.com>
2015-02-23 10:49:35 -0800

    hope this works

--- Main/api/serializers.py
+++ Main/api/serializers.py
@@ -1,8 +1,9 @@
-from Main.models import Track
+from friendship.models import Friend, FriendshipRequest
+from Main.models import Track, UserMusic, Album, Playlist
 
-__author__ = 'danielgin'
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +16,32 @@ class TrackSerializer(serializers.ModelSerializer):
         model = Track
         fields = ('name', 'source', 'album', 'previewURL', 'playlists', 'type')
 
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = ('name', 'artist', 'views', 'likes', 'type')
+
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = ('title', 'user', 'type')
+
+
+class UserMusicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserMusic
+        fields = ('user',)
+
+
+class FriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Friend
+        fields = ('to_user', 'from_user', 'created', 'objects')
+
+
+class FriendshipRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendshipRequest
+        # fields = ('')
