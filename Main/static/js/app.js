22d417d90f968e21a52c852b705bf30602d2f9cc
Daniel Gin <dahgin@gmail.com>
2015-02-26 16:56:41 -0800

    added andrews stuff

--- Main/static/js/app.js
+++ Main/static/js/app.js
@@ -1,4 +1,4 @@
- var echoApp = angular.module('echoApp', ['ngRoute', 'ngResource']);
+ var echoApp = angular.module('echoApp', ['ngRoute', 'ngResource', 'ngCookies', 'audioPlayer-directive']);
 
 echoApp.config(['$httpProvider', function($httpProvider){
         // django and angular both support csrf tokens. This tells
@@ -120,6 +120,85 @@ echoApp.config(['$routeProvider' ,function($routeProvider) {
          $scope.friendslist = friendSort(data);
          //console.log($scope.friendslist)
      });
+
+     echoApp.directive('audioPlayer', function($rootScope) {
+        return {
+            restrict: 'E',
+            scope: {},
+            controller: function($scope, $element) {
+                $scope.audio = new Audio();
+                $scope.currentNum = 0;
+
+                // tell others to give me my prev/next track (with audio.set message)
+                $scope.next = function(){ $rootScope.$broadcast('audio.next'); };
+                $scope.prev = function(){ $rootScope.$broadcast('audio.prev'); };
+
+                // tell audio element to play/pause, you can also use $scope.audio.play() or $scope.audio.pause();
+                $scope.playpause = function(){ var a = $scope.audio.paused ? $scope.audio.play() : $scope.audio.pause(); };
+
+                // listen for audio-element events, and broadcast stuff
+                $scope.audio.addEventListener('play', function(){ $rootScope.$broadcast('audio.play', this); });
+                $scope.audio.addEventListener('pause', function(){ $rootScope.$broadcast('audio.pause', this); });
+                $scope.audio.addEventListener('timeupdate', function(){ $rootScope.$broadcast('audio.time', this); });
+                $scope.audio.addEventListener('ended', function(){ $rootScope.$broadcast('audio.ended', this); $scope.next(); });
+
+                // set track & play it
+                $rootScope.$on('audio.set', function(r, file, info, currentNum, totalNum){
+                    var playing = !$scope.audio.paused;
+                    $scope.audio.src = file;
+                    var a = playing ? $scope.audio.play() : $scope.audio.pause();
+                    $scope.info = info;
+                    $scope.currentNum = currentNum;
+                    $scope.totalNum = totalNum;
+                });
+
+                // update display of things - makes time-scrub work
+                setInterval(function(){ $scope.$apply(); }, 500);
+            }
+        };
+    });
+
+    echoApp.filter('startFrom', function() {
+        return function(input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        };
+    });
+
+    echoApp.controller('MainCtrl', function ($scope, $http, $rootScope) {
+        $scope.currentTrack = 0;
+        $scope.pageSize = 5;
+        $scope.data=[];
+
+        var updateTrack = function(){
+            $rootScope.$broadcast('audio.set', 'mp3/' + $scope.data[$scope.currentTrack].file, $scope.data[$scope.currentTrack], $scope.currentTrack, $scope.data.length);
+        };
+
+        $rootScope.$on('audio.next', function(){
+            $scope.currentTrack++;
+            if ($scope.currentTrack < $scope.data.length){
+                updateTrack();
+            }else{
+                $scope.currentTrack=$scope.data.length-1;
+            }
+        });
+
+        $rootScope.$on('audio.prev', function(){
+            $scope.currentTrack--;
+            if ($scope.currentTrack >= 0){
+                updateTrack();
+            }else{
+                $scope.currentTrack = 0;
+            }
+        });
+
+        $http.get('http://127.0.0.1:8000/api/track/3/')
+            .success(function(response){
+                $scope.data = response;
+                updateTrack();
+            });
+    });
+
 });
 
  echoApp.controller('EchoController', function ($scope, $http) { // calling the app controller up to the http
